e2589000c67221bb95184d8cf2b8747b5773d389
chatembed <ceo@chatembedai.com>
2024-11-04 00:08:01 +0530

    modifications

--- config/django/base.py
+++ config/django/base.py
@@ -29,6 +29,14 @@ THIRD_PARTY_APPS = [
     "ninja_extra",
     "ninja_jwt",
     "ninja_jwt.token_blacklist",
+    "unfold",  # before django.contrib.admin
+    "unfold.contrib.filters",  # optional, if special filters are needed
+    "unfold.contrib.forms",  # optional, if special form elements are needed
+    "unfold.contrib.inlines",  # optional, if special inlines are needed
+    "unfold.contrib.import_export",  # optional, if django-import-export package is used
+    "unfold.contrib.guardian",  # optional, if django-guardian package is used
+    "unfold.contrib.simple_history",  # optional, if django-simple-history package is used
+    "django.contrib.admin",  # required
 ]
 
 PROJECT_APPS = ["core", "authentication"]
